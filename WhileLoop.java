84c70b5a04959265a0644189f0c9604175b7f9bb
Miritievansgitonga <129762323+Miritievansgitonga@users.noreply.github.com>
2024-10-14 17:37:39 +0300

    Add files via upload

---
+++ WhileLoop.java
@@ -0,0 +1,11 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+
+        // While loop to print numbers from 1 to 5
+        while (i <= 5) {
+            System.out.println(i);
+            i++;  // Increment the counter
+        }
+    }
+}
